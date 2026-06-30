ff38e3390c7d05a74a10ac1802bad584822ac73d
Mikolaj0524 <96992608+Mikolaj0524@users.noreply.github.com>
2026-06-30 02:40:02 +0200

    Fixed wrong path

--- backend/utils/paths.js
+++ backend/utils/paths.js
@@ -1,7 +1,7 @@
 import path from "path";
 
 export const data_dir = path.join(process.cwd(), "data")
-export const music_dir =  path.join(data_dir, "/covers");
+export const music_dir =  path.join(data_dir, "/music");
 export const covers_dir =  path.join(data_dir, "/covers");
 export const cache_file =  path.join(data_dir, "/cache.json");
 export const sessions_file =  path.join(data_dir, "/sessions.json");
